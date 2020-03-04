0e69f0c76d9381a9b0e2906af50cd07c4949a268
Titus Fortner <titusfortner@gmail.com>
2020-03-04 20:33:44 +0100

    implement BaseTest

--- src/test/java/com/saucelabs/framework/BrowserActionsTest.java
+++ src/test/java/com/saucelabs/framework/BrowserActionsTest.java
@@ -3,10 +3,9 @@ package com.saucelabs.framework;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class BrowserActionsTest {
+public class BrowserActionsTest extends BaseTest {
     @Test
     public void navigates() {
-        Browser browser = new Browser();
         browser.get("https://www.saucedemo.com/");
         Assert.assertEquals(browser.getCurrentUrl(), "https://www.saucedemo.com/");
         browser.quit();
--- src/test/java/com/saucelabs/framework/ElementCreationTest.java
+++ src/test/java/com/saucelabs/framework/ElementCreationTest.java
@@ -7,25 +7,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ElementCreationTest {
+public class ElementCreationTest extends BaseTest {
     @Test
     public void createsElementFromLocator() {
-        Browser browser = new Browser();
-
         HTMLElement htmlElement = browser.element(By.id("foo"));
-        Assert.assertEquals("org.openqa.selenium.By$ById", htmlElement.getLocator().getClass().getName());
 
-        browser.quit();
+        Assert.assertEquals("org.openqa.selenium.By$ById", htmlElement.getLocator().getClass().getName());
     }
 
     @Test
     public void elementNullBeforeLocate() {
-        Browser browser = new Browser();
-
         HTMLElement htmlElement = browser.element(By.id("foo"));
-        Assert.assertNull(htmlElement.getElement());
 
-        browser.quit();
+        Assert.assertNull(htmlElement.getElement());
     }
 
 
