4eba8ae5d5e0f69f524eb6807e924e9968a3ac4b
Titus Fortner <titusfortner@gmail.com>
2020-03-13 20:19:34 -0500

    discourage public getters for elements

--- src/test/java/com/saucelabs/framework/pages/Login.java
+++ src/test/java/com/saucelabs/framework/pages/Login.java
@@ -6,13 +6,13 @@ import com.saucelabs.framework.elements.TextField;
 import lombok.Getter;
 import org.openqa.selenium.By;
 
-
 public class Login extends PageObject {
-    public String url = "https://www.saucedemo.com/";
-    @Getter protected HTMLElement form = new HTMLElement(browser, By.tagName("form"));
-    @Getter protected TextField username = new TextField(form, By.cssSelector(":first-child"));
-    @Getter protected TextField password = new TextField(browser, By.id("password"));
-    @Getter protected HTMLElement submit = new HTMLElement(browser, By.className("btn_action"));
+    @Getter public String url = "https://www.saucedemo.com/";
+
+    protected HTMLElement form = new HTMLElement(browser, By.tagName("form"));
+    protected TextField username = new TextField(form, By.cssSelector(":first-child"));
+    protected TextField password = new TextField(browser, By.id("password"));
+    protected HTMLElement submit = new HTMLElement(browser, By.className("btn_action"));
 
     public static Login visit() {
         Login login = new Login();
