797f88716edf7d8b26fdbe588f1f05a5ddaf9a72
titusfortner <titusfortner@gmail.com>
2020-03-23 00:43:06 -0500

    have Data Object store keys and get values by String

--- src/test/java/com/saucelabs/framework/data/DataTest.java
+++ src/test/java/com/saucelabs/framework/data/DataTest.java
@@ -4,20 +4,53 @@ import com.saucelabs.framework.resources.UserData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+
 public class DataTest {
     @Test
-    public void createsNewDataObject() {
+    public void createsNewDataObjectWithDefaults() {
         UserData userData = new UserData();
 
+        Assert.assertNotNull(userData.getUsername());
         Assert.assertEquals(String.class, userData.getUsername().getClass());
+        Assert.assertNotNull(userData.getPassword());
         Assert.assertEquals(String.class, userData.getPassword().getClass());
     }
 
     @Test
-    public void createsStaticDataObject() {
+    public void updatesDataObject() {
+        UserData userData = new UserData();
+        userData.setUsername("standard_user");
+        userData.setPassword("secret_sauce");
+
+        Assert.assertEquals("standard_user", userData.getUsername());
+        Assert.assertEquals("secret_sauce", userData.getPassword());
+    }
+
+    @Test
+    public void createsStaticDefinedDataObject() {
         UserData userData = UserData.valid();
 
         Assert.assertEquals("standard_user", userData.getUsername());
         Assert.assertEquals("secret_sauce", userData.getPassword());
+    }
+
+    @Test
+    public void createsKeySet() {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("username");
+        hashSet.add("password");
+
+        UserData userData = new UserData();
+        Assert.assertEquals(hashSet, userData.getKeys());
+    }
+
+    @Test
+    public void getsValueMethod() {
+        UserData userData = new UserData();
+        userData.setUsername("foo");
+
+        String value = (String) userData.getValue("username");
+        Assert.assertEquals("foo", value);
     }
 }
